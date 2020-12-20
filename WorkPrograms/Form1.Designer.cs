1d74fa0252f7cf24c1958f347ca4a653f7c07f9e
20erik03 20erik03 <margiev_e@mail.ru>
2020-12-20 15:33:50 +0300

    Добавьте файлы проекта.

---
+++ WorkPrograms/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace WorkPrograms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonOpenExcel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonOpenExcel
+            // 
+            this.buttonOpenExcel.Location = new System.Drawing.Point(27, 36);
+            this.buttonOpenExcel.Name = "buttonOpenExcel";
+            this.buttonOpenExcel.Size = new System.Drawing.Size(110, 26);
+            this.buttonOpenExcel.TabIndex = 0;
+            this.buttonOpenExcel.Text = "Открыть файл";
+            this.buttonOpenExcel.UseVisualStyleBackColor = true;
+            this.buttonOpenExcel.Click += new System.EventHandler(this.buttonOpenExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.buttonOpenExcel);
+            this.Name = "Form1";
+            this.Text = "WorkPrograms";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonOpenExcel;
+    }
+}
+
