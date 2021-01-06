a2dc308b1aea2156f90810fde722561d84bfe4df
20erik03 20erik03 <margiev_e@mail.ru>
2021-01-06 17:56:22 +0300

    лабораторные

--- WorkPrograms/Form1.cs
+++ WorkPrograms/Form1.cs
@@ -69,6 +69,7 @@ namespace WorkPrograms
             test = "";
             sumLectures = 0;
             sumWorkshops = 0;
+            sumLaboratoryExercises = 0;
             typesOfLessons = "";
             consulting = "";
             interactiveWatch = "";
